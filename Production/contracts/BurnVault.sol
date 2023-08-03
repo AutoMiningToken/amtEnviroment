f20dc0d809f1be178a99f58fdcba743c17c31627
Kegnell <diego.pereira.pck@gmail.com>
2023-08-03 14:01:51 -0300

    Fix I12. Unused Import

--- Production/contracts/BurnVault.sol
+++ Production/contracts/BurnVault.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Amt.sol";
-import "./Master.sol";
 
 /// @title BurnVault
 /// @notice This contract burns AMT tokens and withdraws backing BTCb tokens
