df05fd61e81482898f45a9a64fe8537583232a01
Kegnell <diego.pereira.pck@gmail.com>
2024-02-13 19:54:54 -0300

    Fixed point patch solved Added custom script postinstall to apply patches

--- Production/hardhat.config.ts
+++ Production/hardhat.config.ts
@@ -3,19 +3,6 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
 require("dotenv").config();
 const config: HardhatUserConfig = {
-  networks: {
-    bsc: {
-      url: "https://binance.llamarpc.com",
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    },
-  },
-  etherscan: {
-    apiKey: {
-      bsc: process.env.BSCSCAN_API_KEY
-        ? process.env.BSCSCAN_API_KEY
-        : "undefined",
-    },
-  },
   solidity: {
     compilers: [
       { version: "0.8.9" },
