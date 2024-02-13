fa2bae5817eecf53c84e0ab5cc3f5e7a818bf8eb
Kegnell <diego.pereira.pck@gmail.com>
2024-02-13 20:21:12 -0300

    routes apply patch

---
+++ Testing/apply-patch.js
@@ -0,0 +1,26 @@
+const { exec } = require("child_process");
+const path = require("path");
+
+const patchFilePath = path.join(__dirname, "..", "patches", "FixedPoint.patch");
+const targetDirectory = path.join(
+  __dirname,
+  "..",
+  "node_modules",
+  "@uniswap",
+  "lib",
+  "contracts",
+  "libraries"
+);
+
+const command = `patch -p0 -d "${targetDirectory}" < "${patchFilePath}"`;
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error applying patch: ${error}`);
+    return;
+  }
+  console.log(`Patch applied successfully: ${stdout}`);
+  if (stderr) {
+    console.error(`Patch applied with errors: ${stderr}`);
+  }
+});
